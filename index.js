8d83a1e484d19535e368deb208d18df48bb555fd
Josh <joshuajmv@me.com>
2020-05-05 14:15:32 +0300

    ok

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 var eres1 = document.getElementById('eres-1')
 
+eres1.addEventListener("mouseOver", test);
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
   noCursor();
@@ -13,8 +15,6 @@ background(0,0,20);
 fill(255);
 ellipse(mouseX, mouseY, 33);
 fill(255, 255, 255, 240);
-// ellipse((mouseX+(sin(frameCount*0.05)+1)*3), (mouseY-(sin(frameCount*0.1)+1)*3), (sin(frameCount*0.02)+15)*15);
-ellipse(mouseX, mouseY, (sin(frameCount*0.02)+15)*15);
 }
 
 function test() {
@@ -25,5 +25,3 @@ function test() {
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
-
-eres1.addEventListener("mouseOver", test);
