c46c3c4b64d425cf6ef103c518c636dc94d48a23
Josh <joshuajmv@me.com>
2020-05-05 14:14:18 +0300

    pl

--- index.js
+++ index.js
@@ -1,85 +1,29 @@
 
+var eres1 = document.getElementById('eres-1')
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  noCursor();
+  noStroke();
 }
 
 function draw() {
-	//big tree height
-	var c = 220;
-	//flower size
-	var e = 2;
-	background(0, 0, 20);
-
-	// tree
-	push();
-	translate(0, height);
-		push();
-			translate(width / 2, 0);
-			branch(c, e);
-		pop();
-	pop();
+//mouse
+background(0,0,20);
+fill(255);
+ellipse(mouseX, mouseY, 33);
+fill(255, 255, 255, 240);
+// ellipse((mouseX+(sin(frameCount*0.05)+1)*3), (mouseY-(sin(frameCount*0.1)+1)*3), (sin(frameCount*0.02)+15)*15);
+ellipse(mouseX, mouseY, (sin(frameCount*0.02)+15)*15);
 }
 
-// my functions
-function branch(len, idx) {
-
-	// stop condition
-	if (len < 35) {
-		noStroke();
-		fill(150, 250, 50, 255);
-		ellipse(0, 0, idx, idx);
-		return;
-	}
-
-	// draw the branch
-	strokeWeight(max(1, len / 20));
-	stroke(100, 250, 0, 60);
-	push();
-	line(0, 0, 0, -len);
-
-	// move to the end of it
-	translate(0, -len);
-
-  // branch
-	push();
-	rotate(-noise(frameCount * 0.005, idx * 4) * 1.2);
-	branch(
-		len * ((0.0004*mouseX) + sin(idx * 0.00002, len * frameCount * 0.00001) * 0.54),
-		idx + 2
-	);
-	pop();
-
-  // branch
-  push();
-  rotate(noise(frameCount * 0.005, idx * 4) * 1.2);
-  branch(
-    len * ((0.0005*mouseX) + sin(idx * 0.00002, len * frameCount * 0.00001) * 0.54),
-    idx + 1
-  );
-  pop();
-
-  // branch
-  push();
-  rotate(-noise(frameCount * 0.005, idx * 3) * 1.6);
-  branch(
-    len * ((0.0005*mouseX) + sin(idx * 0.00002, len * frameCount * 0.00001) * 0.99),
-    idx + 1
-  );
-  pop();
-
-  // branch
-  push();
-  rotate(noise(frameCount * 0.005, idx * 4) * 0.7);
-  branch(
-    len * ((0.00051*mouseX) + sin(idx * 0.00003, len * frameCount * 0.00002) * 10),
-    idx + 1
-  );
-  pop();
-
-	// move back to root of this branch
-	pop();
+function test() {
+  print("ok");
+  return;
 }
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
+
+eres1.addEventListener("mouseOver", test);
